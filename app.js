502c5cf1af91cee2f4ea2f7a54ba5f7c5630ee44
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2024-01-31 20:38:01 +0500

    fix: port issue

--- app.js
+++ app.js
@@ -45,5 +45,4 @@ mongoose
 
 // Start the server
 
-const port = 3001;
-app.listen(port, () => console.log(`running on port ${port}`));
+app.listen(process.env.PORT || 5000, () => console.log(`running on port ${process.env.PORT || 5000}`));
